5ea3ca235c6bb912f0773946b743ac5af9bf72a5
Ubuntu <ubuntu@ip-172-31-41-75.ec2.internal>
2017-11-08 06:23:53 +0000

    Increasing db char size

--- ManagerUI/app/ec2_examples.py
+++ ManagerUI/app/ec2_examples.py
@@ -189,7 +189,7 @@ def ec2_delete_all():
         cursor.execute("drop table images;")
         cursor.execute("drop table users;")
         cursor.execute("CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT, username char(100) NOT NULL, passhash char(100) NOT NULL, passsalt char(100) NOT NULL, PRIMARY KEY (id)) ENGINE = InnoDB;")
-        cursor.execute("CREATE TABLE images(userid INT, imagename char(100) NOT NULL, orig char(100) NOT NULL, redblueshift char(100) NOT NULL, grayscale char(100) NOT NULL, overexposed char(100) NOT NULL, INDEX par_ind (userid), FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE = InnoDB;")
+        cursor.execute("CREATE TABLE images(userid INT, imagename char(200) NOT NULL, orig char(200) NOT NULL, redblueshift char(200) NOT NULL, grayscale char(200) NOT NULL, overexposed char(200) NOT NULL, INDEX par_ind (userid), FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE = InnoDB;")
         cnx.commit()
     except:
         cnx.rollback
