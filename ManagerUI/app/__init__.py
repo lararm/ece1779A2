31278ece4afd630ae4dbab3353f17b540f9b8f03
Ubuntu <ubuntu@ip-172-31-41-75.ec2.internal>
2017-11-12 19:41:06 +0000

    Adding init changes

--- ManagerUI/app/__init__.py
+++ ManagerUI/app/__init__.py
@@ -1,15 +1,15 @@
 import threading
 from threading import Thread
 from flask import Flask
-from app import cloudWatch
+from app import autoscale
 
 webapp = Flask(__name__)
 
-from app import ec2_examples
+from app import manager
 from app import s3_examples
 from app import main
 
-auto_scale = Thread(target= cloudWatch.get_instances_cpu_avg)
+auto_scale = Thread(target= autoscale.get_instances_cpu_avg)
 auto_scale.start()
 
 #athreading.Timer(60.00, cloudWatch.get_instances_cpu_avg).start()
